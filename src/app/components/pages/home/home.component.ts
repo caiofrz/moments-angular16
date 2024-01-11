a42fae9e725bf75043e1ff667d1f38cf8ba91ecb
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-11 16:48:50 -0300

    feat(Moment - Home page): add search moments

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, timeout } from 'rxjs';
 import { Moment } from 'src/app/interfaces/Moments';
 import { MomentService } from 'src/app/services/moment.service';
 import { environment } from 'src/environments/environment';
@@ -18,17 +19,30 @@ export class HomeComponent implements OnInit {
   constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
-    this.momentService.getAll().subscribe((moments) => {
-      const data = moments.data;
-      data.map((moment) => {
-        moment.created_at = new Date(moment.created_at!).toLocaleDateString(
-          'pt-BR'
-        );
-      });
+    setInterval(
+      () =>
+        this.momentService.getAll().subscribe((moments) => {
+          const data = moments.data;
+          data.map((moment) => {
+            moment.created_at = new Date(moment.created_at!).toLocaleDateString(
+              'pt-BR'
+            );
+          });
 
-      this.moments = data;
-      this.momentsSearch = data;
-      this.loading = false;
+          this.moments = data;
+          this.momentsSearch = data;
+          this.loading = false;
+        }),
+      5000
+    );
+  }
+
+  search(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const searchValue = target.value;
+
+    this.moments = this.momentsSearch.filter((moment) => {
+      return moment.title.toLowerCase().includes(searchValue.toLowerCase());
     });
   }
 }
