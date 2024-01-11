73c2e630d6cff402c8ffb6ecf3c41169f9a87472
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-11 20:23:32 -0300

    feat(header): aplly responsivity

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  class: string = '';
+  showMenu() {
+    this.class = this.class === '' ? 'responsive': '';
+  }
 }
